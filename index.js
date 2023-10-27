7b9030c101c682282b1a107a01571ad972598043
hug0exe <70762332+hug0exe@users.noreply.github.com>
2023-10-27 13:54:36 +0200

    add sound

---
+++ index.js
@@ -0,0 +1,16 @@
+// Import Howler.js
+import { Howl, Howler } from 'howler';
+
+// Create a Howl instance for your sound
+const sound = new Howl({
+    src: ['/assets/mp3/barkconverterino.mp3'], // Replace with the actual path
+});
+
+// Get a reference to the button element
+const playSoundButton = document.getElementById('playSoundButton');
+
+// Add a click event listener to the button
+playSoundButton.addEventListener('click', () => {
+    // Play the sound when the button is clicked
+    sound.play();
+});
