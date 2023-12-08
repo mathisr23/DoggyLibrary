c857d339e4621c957598409c841e1df2e52ef9b4
Rivière Mathis <mathis.riviere@edu.devinci.fr>
2023-12-08 10:33:46 +0100

    fixed loader but no music ITS OKAY

--- js/loaderTwerk.js
+++ js/loaderTwerk.js
@@ -1,39 +1,47 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const dogLoader = document.getElementById("dogLoader")
+  const dogLoader = document.getElementById("dogLoader");
 
   if (dogLoader) {
-    const loaderCircle = document.getElementById("loader-circle")
-    const content = document.querySelector(".content")
-    const counter = document.getElementById("counter")
-    const loaderImage = document.querySelector(".loader-image")
-    const loadingAudio = document.getElementById("loadingAudio")
+    const loaderCircle = document.getElementById("loader-circle");
+    const content = document.querySelector(".content");
+    const counter = document.getElementById("counter");
+    const loaderImage = document.querySelector(".loader-image");
+    const loadingAudio = document.getElementById("loadingAudio");
 
     // Function to play audio once
     function playAudioOnce() {
-      loadingAudio.play()
+      loadingAudio.play();
       // Remove the event listener after the first play
-      document.removeEventListener("click", playAudioOnce)
+      document.removeEventListener("DOMContentLoaded", playAudioOnce);
     }
 
     // Add the event listener to play audio once when the page loads
-    document.addEventListener("click", playAudioOnce)
+    document.addEventListener("DOMContentLoaded", playAudioOnce);
 
-    let count = 0
+    let count = 0;
 
-    // Mettez à jour le compteur toutes les 50 millisecondes
+    // Update the counter every 50 milliseconds
     const interval = setInterval(function () {
-      count += 1
-      counter.innerText = count
+      count += 1;
+      counter.innerText = count;
 
       if (count >= 100) {
-        clearInterval(interval)
+        clearInterval(interval);
 
-        // Masquez le cercle de chargement, le compteur et le GIF, affichez le contenu
-        loaderCircle.style.display = "none"
-        counter.style.display = "none"
-        loaderImage.style.display = "none"
-        content.style.opacity = 1
+        // Hide the loader circle, counter, and GIF; display the content
+        if (loaderCircle) {
+          loaderCircle.style.display = "none";
+        }
+        if (counter) {
+          counter.style.display = "none";
+        }
+        if (loaderImage) {
+          loaderImage.style.display = "none";
+        }
+        if (content) {
+          content.style.opacity = 1;
+        }
       }
-    }, 50) // Ajustez la fréquence de mise à jour du compteur selon vos besoins
+    }, 50); // Adjust the update frequency according to your needs
   }
-})
+});
